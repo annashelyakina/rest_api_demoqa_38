8cac63a6ffa4f0f12e5e2f30875c370e97f74826
annashelyakina <anna.shelyakina82@gmail.com>
2025-12-05 00:06:29 +0300

    added TestBase and TestData

---
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -0,0 +1,24 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup(){
+        Configuration.baseUrl = "https://demoqa.com";
+        RestAssured.baseURI="https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @AfterEach
+    void shutDown(){
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -0,0 +1,10 @@
+package com.demoqa.tests;
+
+public class TestData {
+
+    public static String
+            login ="annatestova38",
+            password ="Anna#Testova_38",
+            isbn = "9781449365035";
+
+}
