385c1164fa5b7b3b6a67e082935469247a2fd63d
annashelyakina <anna.shelyakina82@gmail.com>
2025-12-05 00:08:34 +0300

    added helpers and models

---
+++ src/test/java/com/demoqa/models/AddBooksListModel.java
@@ -0,0 +1,13 @@
+package com.demoqa.models;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AddBooksListModel {
+
+    String userId;
+
+    List<IsbnModel> collectionOfIsbns;
+}
